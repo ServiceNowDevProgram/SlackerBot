9eb69f15a8952e5948a87ed12b337d49b5b5c38e
Earl Duque <31702109+earlduque@users.noreply.github.com>
2022-09-27 23:29:48 -0700

    Update Clap back.js

--- Parsers/Clap back.js
+++ Parsers/Clap back.js
@@ -5,7 +5,7 @@ flags:gmi
 */
 var sentence = current.text.replace(/!clap/gmi, "").trim().toUpperCase();
 if (sentence == '') {
-	var send_confusion = new SlackFall().send_chat(current.channel, ':upside_down_face: gimme something to clap!', false, '', current.thread_ts);
+	var send_confusion = new x_snc_slackerbot.Slacker().send_chat(current.channel, ':upside_down_face: gimme something to clap!', false, '', current.thread_ts);
 } else {
 	new x_snc_slackerbot.Slacker().send_chat(current, sentence.split(' ').join(' :clap: '), current.thread_ts);
 }

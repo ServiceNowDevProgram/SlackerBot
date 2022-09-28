40893b5916b462f8b98e43acb2129c6538ffd873
Earl Duque <31702109+earlduque@users.noreply.github.com>
2022-09-27 21:07:53 -0700

    updating multiple files

--- Parsers/Clap back.js
+++ Parsers/Clap back.js
@@ -3,7 +3,7 @@ activation_example:!clap your sentence
 regex:!clap
 flags:gmi
 */
-var sentence = current.text.replace(/!clap/gmi, "").trim().toUpperCase(); //
+var sentence = current.text.replace(/!clap/gmi, "").trim().toUpperCase();
 if (sentence == '') {
 	var send_confusion = new SlackFall().send_chat(current.channel, ':upside_down_face: gimme something to clap!', false, '', current.thread_ts);
 } else {

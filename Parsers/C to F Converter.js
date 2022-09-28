2f0a7bbe1d9d9ab477c85dc6bc6c8790881c0796
Earl Duque <31702109+earlduque@users.noreply.github.com>
2022-09-27 22:38:07 -0700

    new parsers

---
+++ Parsers/C to F Converter.js
@@ -0,0 +1,12 @@
+/*
+activation_example:30c or 30 degrees celcius
+regex:(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?c(?:elsius|elcius)?\b
+flags:gmi
+*/
+var regextest = /(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?c(?:elsius|elcius)?\b/gmi;
+var match = regextest.exec(current.text);
+var numbertest = /-?\d{1,}\.?\d{0,}/;
+var numbermatch = numbertest.exec(match[0]);
+var ctof = (parseFloat(numbermatch[0]) * 9/5) + 32;
+var originalnumber = parseFloat(numbermatch[0]).toFixed(2).toString().slice(-3) == '.00' ? parseFloat(numbermatch[0]).toFixed(2).toString().slice(0, -3) : parseFloat(numbermatch[0]).toFixed(2);
+var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, originalnumber + 'C is ' + ctof.toFixed(2) + ' degrees in freedom units (Fahrenheit).');
---
+++ Parsers/International Space Station.js
@@ -0,0 +1,42 @@
+/*
+activation_example:!iss
+regex:!iss
+flags:gmi
+*/
+var get_coords = new x_snc_slackerbot.ISS().get_coords();
+var iss_lat = get_coords.iss_position.latitude;
+var iss_long = get_coords.iss_position.longitude;
+
+// 	Los Angeles: 34.0522,118.2437
+//  	iss_lat = '34.0522';
+//  	iss_long = '118.2437';
+
+
+var get_location = new x_snc_slackerbot.ISS().get_location(iss_lat, iss_long);
+gs.info('slacker get_location: ' + JSON.stringify(get_location));
+var location = '';
+if (get_location.status == 'ZERO_RESULTS') location = 'the ocean.';
+else {
+	// 		var area1 = get_location.results.address_components[6].long_name;
+	// 		var area2 = get_location.results.address_components[7].long_name;
+	// 		gs.info('slacker: ' + get_location.results.address_components);
+	var area1 = ''; var area2 = '';
+	for (var i in get_location.results[0].address_components){
+		if (get_location.results[0].address_components[i].types[0] == 'administrative_area_level_1') area1 = get_location.results[0].address_components[i].long_name;
+		if (get_location.results[0].address_components[i].types[0] == 'administrative_area_level_2') area2 = get_location.results[0].address_components[i].long_name;
+		else if (get_location.results[0].address_components[i].types[0] == 'country') area2 = get_location.results[0].address_components[i].long_name;
+	}
+	if (!area1 && !area2) location = 'the ocean';
+	else if (area1) location = area1 + ', ' + area2 + '.';
+	else location = area2 + '.';
+}
+
+var get_image = new x_snc_slackerbot.ISS().get_image(iss_lat, iss_long);
+
+//var area1 = 'test1';
+//var area2 = 'test2';
+
+// 	var send_chat = new SlackFall().send_chat(current.channel, 'The International Space Station is currently above latitude: ' + get_coords.iss_position.latitude + ', longitute: ' + get_coords.iss_position.longitude + '\nThose coordinates place the ISS over ' + location, false, '', current.thread_ts);
+
+// 	send_attachment: function(channel, message, image, thread, pretext)
+var send_chat = new x_snc_slackerbot.Slacker().send_attachment(current, 'The International Space Station is currently above latitude: ' + iss_lat + ', longitude: ' + iss_long + '\nThose coordinates place the ISS over ' + location, get_image, current.thread_ts, true);

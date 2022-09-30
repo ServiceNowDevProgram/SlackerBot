3066efe2b86da5faea22573cdc88c658c8aff5b6
wiz0floyd <wiz0floyd@gmail.com>
2022-09-30 11:42:37 -0400

    Update wikipedia.js

--- Parsers/wikipedia.js
+++ Parsers/wikipedia.js
@@ -4,11 +4,11 @@ regex:!wiki
 flags:gmi
 */
 
-var term = /(?:!wiki )(.*)/gmi.exec(current.text)[1];
+var term = /(?:!wiki )(.*)/gmi.exec(current.text);
+gs.info("WIKI: " + term);
+
 if (term == null) {
 	var send_confusion = new x_snc_slackerbot.Slacker().send_chat(current, ':upside_down_face: !wiki *something*', false);
 } else {
-	
-	gs.info("WIKI: " + term);
-	var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, 'https://en.wikipedia.org/w/index.php?title=Special%3ASearch&search=' + escape(term) , false);
+	var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, 'https://en.wikipedia.org/w/index.php?title=Special%3ASearch&search=' + escape(term[1]) , false);
 }

07dccecc5c06418dd25be2ad6c280b5b83330847
wiz0floyd <wiz0floyd@gmail.com>
2022-09-30 11:34:59 -0400

    Wikipedia Parser

---
+++ Parsers/wikipedia.js
@@ -0,0 +1,14 @@
+/*
+activation_example:!wiki ServiceNow
+regex:!wiki
+flags:gmi
+*/
+
+var term = /(?:!wiki )(.*)/gmi.exec(current.text)[1];
+if (term == null) {
+	var send_confusion = new x_snc_slackerbot.Slacker().send_chat(current, ':upside_down_face: !wiki *something*', false);
+} else {
+	
+	gs.info("WIKI: " + term);
+	var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, 'https://en.wikipedia.org/w/index.php?title=Special%3ASearch&search=' + escape(term) , false);
+}

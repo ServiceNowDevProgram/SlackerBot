ee63a3ff5fb14819c03d192a0ae716d3ee163d6a
wiz0floyd <wiz0floyd@gmail.com>
2022-09-30 17:52:23 -0400

    Added bowtie reaction

--- Parsers/Bowtie when chuck is mentioned.js
+++ Parsers/Bowtie when chuck is mentioned.js
@@ -7,7 +7,7 @@ new x_snc_slackerbot.Slacker().send_reaction(current, chuckMoji());
 
 
 function chuckMoji(){
-    var chucks = ['chuck-le','chuck-point','thinking-chuck','upsidedown-chuck']
+    var chucks = ['chuck-le','chuck-point','thinking-chuck','upsidedown-chuck','thinking-chuck-cartoon','bowtie']
 
     return chucks[parseInt(Math.random() * chucks.length)];
     

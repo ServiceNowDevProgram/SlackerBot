aca747e7875a2af4a63b3207e739b666f8a961d2
wiz0floyd <wiz0floyd@gmail.com>
2022-09-30 17:46:49 -0400

    Updated chuck react

--- Parsers/Bowtie when chuck is mentioned.js
+++ Parsers/Bowtie when chuck is mentioned.js
@@ -3,4 +3,12 @@ activation_example:@Chuck.Tomasi
 regex:<@U43AA4W64>
 flags:g
 */
-new x_snc_slackerbot.Slacker().send_reaction(current, 'chuck-le');
+new x_snc_slackerbot.Slacker().send_reaction(current, chuckMoji());
+
+
+function chuckMoji(){
+    var chucks = ['chuck-le','chuck-point','thinking-chuck','upsidedown-chuck']
+
+    return chucks[parseInt(Math.random() * chucks.length)];
+    
+}

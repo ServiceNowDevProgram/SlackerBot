4b271adf1b2e7bd56039e800ab363854181f07d2
John Dahl <john.dahl@ajuvo.com>
2022-10-01 11:01:04 -0500

    Added the Usage parser

---
+++ Parsers/usage.js
@@ -0,0 +1,36 @@
+/*
+activation_example:!usage
+regex:^(!usage|!report)
+flags:i
+*/
+
+var count, message, query, report_on, regex;
+
+current = { 'text': '!usage' };
+
+regex = /^(!usage|!report) (.*)/i;
+report_on = current.text.match( regex ) ? current.text.match( regex )[2] : null;
+
+if ( !report_on ) {
+    message = ':upside_down_face: !usage *something*';
+    return new x_snc_slackerbot.Slacker().send_chat( current, message, false );
+}
+
+query = [
+        'textLIKE' + report_on,
+        '^sys_created_onONLast 7 days@javascript:gs.beginningOfLast7Days()@javascript:gs.endOfLast7Days()'
+    ].join( '' );
+
+count = new global.GlideQuery.parse( 'x_snc_slackerbot_chat', query ).count();
+
+message = [
+        'The message `',
+        report_on,
+        '` has appeared ',
+        count,
+        ' times in the channels I\'ve been watching in the last seven days.'
+    ].join( '' );
+
+return new x_snc_slackerbot.Slacker().send_chat( current, message, false );
+
+

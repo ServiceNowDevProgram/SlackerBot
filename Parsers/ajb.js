f34daeffa5daf01d76937d6295b9bc5afbcb18ac
DavidArthurCole <40234707+DavidArthurCole@users.noreply.github.com>
2022-10-01 01:45:33 -0400

    Re-create !ajb Parser
    Re-create and heavily optimize the !ajb parser
    close #6

---
+++ Parsers/ajb.js
@@ -0,0 +1,29 @@
+/*
+activation_example:!ajb 5
+regex:!ajb
+flags:gm
+*/
+
+var choices = [
+    ':ajb:',
+    ':partyajb:',
+    ':mobilityajb:',
+    ':ajbconga:',
+    ':ajbgoespoof:',
+    ':ajbleftfall:',
+    ':ajbdownfall:',
+    ':ajbispumped:',
+    ':ajbdescends:',
+    ':ajbupsidedown:',
+    ':ajbheadswillroll:'
+];
+
+//Store the numerical value of the second split
+var split = parseInt((current.text.split(' ')[1]), 10);
+var text;
+
+//Determine text to send
+if(!split || split == '' || split <= 0) text = "You're not going to get any :ajb: with that attitude! Add a number and try again.";
+else text = (split > 10 ? 'Now that would just be rude. Shame on you' : (split > 0 ? '' : choices.join('').repeat(split)));
+
+var sendIt = new x_snc_slackerbot.Slacker().send_chat(current, text, false);

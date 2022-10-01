7b0215092316c7e902dbc51df2988d31665bc623
Earl Duque <31702109+earlduque@users.noreply.github.com>
2022-10-01 00:19:24 -0700

    Update All Commands.js

--- Parsers/All Commands.js
+++ Parsers/All Commands.js
@@ -9,6 +9,9 @@ parser.orderBy('activation_example');
 parser.query();
 var helps = [];
 while(parser.next()){
-	helps.push('`' + (parser.getValue('activation_example') ? parser.getValue('activation_example') : 'React with ' + parser.getValue('emoji')) + '` ' + parser.getValue('description'));
+	if (parser.getValue('description').indexOf('Admin tool') == -1 || 
+		(parser.getValue('description').indexOf('Admin tool') > -1 (current.channel == "GD51HTR46" || current.channel == "G9LAJG7G8" || current.channel == "G7M4AP6U8"))) {
+		helps.push('`' + (parser.getValue('activation_example') ? parser.getValue('activation_example') : 'React with ' + parser.getValue('emoji')) + '` ' + parser.getValue('description'));
+	}
 }
 new Slacker().send_chat(current, helps.join('\n'), true);

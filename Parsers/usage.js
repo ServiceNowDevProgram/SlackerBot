2717b842291e64458d71ba86b9017dbfcb0b5e00
John Dahl <103326903+johndahl-now@users.noreply.github.com>
2022-10-01 16:09:26 -0500

    Added else clause

--- Parsers/usage.js
+++ Parsers/usage.js
@@ -13,22 +13,22 @@ if ( !report_on ) {
     message = ':upside_down_face: !usage *something*';
     new x_snc_slackerbot.Slacker().send_chat( current, message, false );
 }
+else{
+    query = [
+            'textLIKE' + report_on,
+            '^sys_created_onONLast 7 days@javascript:gs.beginningOfLast7Days()@javascript:gs.endOfLast7Days()'
+        ].join( '' );
 
-query = [
-        'textLIKE' + report_on,
-        '^sys_created_onONLast 7 days@javascript:gs.beginningOfLast7Days()@javascript:gs.endOfLast7Days()'
-    ].join( '' );
+    count = new global.GlideQuery.parse( 'x_snc_slackerbot_chat', query ).count();
 
-count = new global.GlideQuery.parse( 'x_snc_slackerbot_chat', query ).count();
+    message = [
+            'The message `',
+            report_on,
+            '` has appeared ',
+            count,
+            ' times in the channels I\'ve been watching in the last seven days.'
+        ].join( '' );
 
-message = [
-        'The message `',
-        report_on,
-        '` has appeared ',
-        count,
-        ' times in the channels I\'ve been watching in the last seven days.'
-    ].join( '' );
-
-new x_snc_slackerbot.Slacker().send_chat( current, message, false );
-
+    new x_snc_slackerbot.Slacker().send_chat( current, message, false );
+}
 

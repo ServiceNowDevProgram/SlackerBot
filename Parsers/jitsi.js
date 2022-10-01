4e4e4ff04afb9895e8382dba1119ed261ff1f0a8
Earl Duque <31702109+earlduque@users.noreply.github.com>
2022-09-30 21:59:57 -0700

    Update jitsi.js

--- Parsers/jitsi.js
+++ Parsers/jitsi.js
@@ -29,7 +29,7 @@ response = response || [
 ].join( '' );
 
 try{
-    new x_snc_slackerbot.Slacker().send_chat( current, response, current.thread_ts );
+    new x_snc_slackerbot.Slacker().send_chat( current, response, false );
 }
 catch( e ){
     gs.error( "An error occured when SlackerBot tried to send a response back to Slack.\nError: " + e.name + ": " + e.message );

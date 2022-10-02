904f0c2ca6a8493d9e22376985ddf5b309669d95
John Dahl <103326903+johndahl-now@users.noreply.github.com>
2022-10-02 04:30:56 -0500

    Removed original logic

--- Parsers/Show all SlackerBot commands.js
+++ Parsers/Show all SlackerBot commands.js
@@ -4,18 +4,6 @@ regex:!help
 flags:gmi
 */
 
-// var parser = new GlideRecord('x_snc_slackerbot_parser');
-// parser.orderBy('activation_example');
-// parser.query();
-// var helps = [];
-// while(parser.next()){
-// 	if (parser.getValue('description').indexOf('Admin tool') == -1 || 
-// 		(parser.getValue('description').indexOf('Admin tool') > -1 && (current.channel == "GD51HTR46" || current.channel == "G9LAJG7G8" || current.channel == "G7M4AP6U8"))) {
-// 		helps.push('`' + (parser.getValue('activation_example') ? parser.getValue('activation_example') : 'React with ' + parser.getValue('emoji')) + '` ' + parser.getValue('description'));
-// 	}
-// }
-// new Slacker().send_chat(current, helps.join('\n'), true);
-
 var message = new global.GlideQuery( 'x_snc_slackerbot_parser' )
 .where( 'active', true )
 .orderBy( 'activation_example' )
@@ -32,4 +20,4 @@ if (current.channel == 'G9LAJG7G8' || current.channel == 'GD51HTR46') {
     message.push( '`!send !channel !message` forces this bot to send that message to that channel' );
 }
 
-new Slacker().send_chat( current, message.join('\n'), false );
+new Slacker().send_chat( current, message.join('\n'), true );

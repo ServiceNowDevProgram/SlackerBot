1f7bbebd21edf4044c5b75ca7a0d8311ecdc2f24
Gautham <gauthambellamkonda@gmail.com>
2022-10-04 18:37:13 +0530

    Fix linking to channels

--- Parsers/Hacktoberfest.js
+++ Parsers/Hacktoberfest.js
@@ -3,9 +3,5 @@ activation_example:!hacktoberfest
 regex:!hacktoberfest
 flags:gmi
 */
-
-new x_snc_slackerbot.Slacker().send_chat(
-  current,
-  "Hacktoberfest: a month-long celebration of open-source projects,  their maintainers, and the entire community of contributors.\n For more information, please see: https://hacktoberfest.com/, https://developer.servicenow.com/blog.do?p=/post/hacktoberfest-2022/, and check out our channels: #hacktoberfest, #slacker-dev-public",
-  current.thread_ts
-);
+var message = 'Hacktoberfest: a month-long celebration of open-source projects,  their maintainers, and the entire community of contributors.\n For more information, please see: https://hacktoberfest.com/, https://developer.servicenow.com/blog.do?p=/post/hacktoberfest-2022/, and join us at our channels: <#C01C0RWV32M|hacktoberfest>, <#CKJ2TE0AK|slacker-dev-public>'
+var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, message, current.thread_ts);

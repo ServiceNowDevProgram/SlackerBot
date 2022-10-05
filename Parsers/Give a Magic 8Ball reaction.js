6403961febf3dc0aaf1327cad61f9f678e56deb9
Mattias Johnsson <mjonssonme@users.noreply.github.com>
2022-10-05 23:15:22 +0200

    DOne

---
+++ Parsers/Give a Magic 8Ball reaction.js
@@ -0,0 +1,28 @@
+/*
+emoji:8ball
+*/
+
+var responses = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+var randMagic8 = responses[parseInt(Math.random() * responses.length)];
+var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, randMagic8, true);

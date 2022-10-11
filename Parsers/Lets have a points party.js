9b1618a822cc5d9e75efdeb863c95f0ba34409b2
John Dahl <john.dahl@ajuvo.com>
2022-10-10 20:20:05 -0500

    Refactored to give individual points to each user.

--- Parsers/Lets have a points party.js
+++ Parsers/Lets have a points party.js
@@ -4,112 +4,42 @@ regex:^!pointsparty
 flags:i
 */
 
-(function( grChat ){
-    var channel, messages, participant_slack_ids, text, thread_ts, user;
+( function( current ){
+	// Given a Chat GlideRecord, if it's threaded, then gather all
+  // Slack user IDs and post a point message for each.
+  var grChat, message, recipients, thread_ts, user_id, user_sysid;
+  var si = new Slacker();
+  
+  thread_ts = current.getValue( 'thread_ts' );
+  user_id = current.user.getRefRecord().getValue( 'user_id' );
+  user_sysid = current.getValue( 'user' );
+  recipients = [];
+  
+  if( !thread_ts ){
+    message = 'You cannot throw a points party in public. Get a thread!';
+    si.send_chat( current, message, false );
+    return null;
+  }
 
-    gs.info( 'Slacker: Starting the PointsParty parser.' );
+  grChat = new GlideRecord( 'x_snc_slackerbot_chat' );
+  grChat.addQuery( 'thread_ts', thread_ts );
+  grChat.addQuery( 'user', '!=', user_sysid );
+  grChat.query();
+  
+  while( grChat.next() ){
+    recipients.push( grChat.user.getRefRecord().getValue( 'user_id' ) );
+  }
+  
+  recipients = recipients.filter( function( recipient, idx, arr ){
+    return arr.indexOf( recipient ) == idx;
+  } );
+  
+  message = ['<@' + user_id + '> is throwing a points party for :\n'];
+  
+  recipients.forEach( function( recipient ){
+    message.push( '<@' + recipient + '> ++ \n' );
+  } );
+  
+  si.send_chat( current, message.join(''), false );
 
-    if( !grChat.thread_ts ){ // Do not throw party unless in a thread
-        return null;
-    }
-
-    user = grChat.getValue( 'user' );
-    channel = grChat.getValue( 'channel' );
-    thread_ts = grChat.getValue( 'thread_ts' );
-    text = grChat.getValue( 'text' );
-    messages = [];
-
-    gs.info( 'Slacker: Gather thread participants.' );
-    
-    participant_slack_ids = new global.GlideQuery( 'x_snc_slackerbot_chat' )
-        .where( 'channel', channel )
-        .where( 'thead_ts', thread_ts )
-        .where( 'user', '!=', user )
-        .select( 'user' )
-        .reduce( function( arr, rec ){
-            arr.push( rec.user );
-            return arr;
-        }, [] )
-        .filter( function( rec, idx, arr ){
-            return arr.indexOf( rec ) == idx;
-        } );
-
-    gs.info( 'Slacker: Participant IDs: ' + JSON.stringify( participant_slack_ids ) );
-    
-
-    participant_slack_ids.forEach( function( slack_id ){
-        updateUserTable( slack_id );
-        updatePointsTable( user, slack_id, grChat.getUniqueValue(), text );
-        var rankAndPoints = calculateRankAndPoints( slack_id );
-        messages.push( getMessage( slack_id, rankAndPoints[1], rankAndPoints[0] ) );
-    } );
-
-    gs.info( 'Slacker: Messages: ' + JSON.stringify( messages ) );
-    
-    new Slacker().send_chat( grChat, messages.join('\n'), false );
-
-
-    function updateUserTable( user ){
-        gs.info( 'Slacker: Starting to update the User table.' );
-    
-        var grUser = new GlideRecord( 'x_snc_slackerbot_user' );
-        grUser.addQuery( 'user_id', user );
-        grUser.query();
-
-        if( grUser.next() ){
-            grUser.setValue( 'points', parseInt( grUser.getValue( 'points' ) ) + 1 );
-            grUser.update();
-        }
-        else{
-            grUser.initialize();
-            grUser.setValue( 'user_id', user );
-            grUser.setValue( 'points', 1 );
-            grUser.insert();
-        }
-    }
-
-    function updatePointsTable( giver, target, source_event, text ){
-        gs.info( 'Slacker: Starting to update the Points table.' );
-    
-        var grPointRecord = new GlideRecord( 'x_snc_slack_points_point' );
-        grPointRecord.initialize();
-        grPointRecord.setValue( 'giver', giver );
-        grPointRecord.setValue( 'target', target );
-        grPointRecord.setValue( 'source_event', source_event );
-        grPointRecord.setValue( 'text', text );
-        grPointRecord.insert();
-    }
-
-    function calculateRankAndPoints( user ){
-        gs.info( 'Slacker: Starting to calculate rank and total points.' );
-    
-        var last90days, points, rank, userPoints, users;
-        
-        users = [];
-        last90days = 'sys_created_onONLast 90 days@javascript:gs.beginningOfLast90Days()@javascript:gs.endOfLast90Days()';
-        points = new global.GlideQuery.parse( 'x_snc_slack_points_point', last90days )
-        .aggregate( 'count' )
-        .groupBy( 'target' )
-        .orderByDesc( 'count' )
-        .select()
-        .reduce( function( arr, rec ){
-            users.push( rec.group.target );
-            arr.push( rec.count );
-            return arr;
-        }, [] );
-
-        rank = users.indexOf( user ) + 1;
-        userPoints = points[ rank - 1 ];
-
-        return [ rank, userPoints ];
-    }
-
-    function getMessage( user, points, rank ){
-        gs.info( 'Slacker: Starting to get the message.' );
-    
-        var randomMessage = new x_snc_slack_points.RandomMessage();
-        return randomMessage.getMessage( user, userPoints, userPoints, rank );
-    }
-
-
-})( current );
+} )( current );

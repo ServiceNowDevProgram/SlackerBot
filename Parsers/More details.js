8d1f0046f6db6b62728a6b421735e8aa0c81eced
Badal <69868645+BadalHub@users.noreply.github.com>
2022-10-12 09:48:46 +0530

    Update More details.js

--- Parsers/More details.js
+++ Parsers/More details.js
@@ -1,1 +1,9 @@
+/*
+activation_example: Says the word clear elaborate
+regex: clear
+flags:g
+*/
+ 
+
+
 new x_snc_slackerbot.Slacker().send_chat(current,'Can you please be more detailed on this!');

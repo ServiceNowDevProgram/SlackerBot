6004db791ab0a05583591d1bd3d7fbde8a91c7b8
Brian Godfrey <bg@briangodfrey.dev>
2022-10-11 21:24:42 -0400

    Create I want to be big - Ask Zoltar.js
    Version .10
    
    Left to do:
    1. Add remaining futures
    2. Add the display of a gif or jpeg prior to future
    3. Add delay such that someone has time to make a wish

---
+++ I want to be big - Ask Zoltar.js
@@ -0,0 +1,73 @@
+/*
+activation_example:!zoltar
+regex:!zoltar
+flags:gmi
+*/
+
+var zoltarOpen = [
+	
+	"Come closer and hear what Zoltar has to tell you...",
+	
+	"It's wise to be taking good advice when you hear it, no?",
+	
+	"Words of wisdom to bring you much happiness!",
+	
+];
+
+var futures = [
+	
+    "YOUR WISH IS GRANTED! \n\nLucky Numbers: 08, 14, 16, 17, 39, 43",
+
+    "It's time to stop waiting for things to get better when you're feeling somber. Rather, start making them better. Start with perspective. When you are sad, know that things will soon turn around. When you are happy, don't get overzealous and know that it can be taken from you in an instant. It is important to not let these changes destroy you but accept them and know that life is a constant wave of joy and loss. Don't let the waves knock you down, fight through them and the entire ocean will be yours. \n\nLucky Numbers: 16, 27, 33, 10, 22, 43",
+
+    "Are you tired, are you weary? Does life feel drab and dreary? Despair not of this my friend I see happiness around the bend. \n\n Say farewell to those blues you have been nursing. Get in the habit of looking at the brighter side of life. You have a tempermental nature. You lose your temper easily, but regret it just as fast. You have a brilliant mind and enjoy reading and the fine arts. Your friends appreciate your intelligence and lide to come to you with their problems. You have a very sympathetic nature. A dark haired person who is trying to harm you will soon disappear from your life and you will be extremely happy. \n\nLucky Numbers: 06, 08, 29, 40, 46, 48",
+
+    "You've been wondering if you have spent your time wisely. A person that is young in years may be old in hours if he has lost no time. If you feel old, then you know you have not wasted any time. If you feel young, then do not waste the day before you now. Remember a day is a fortune and an estate if you lose a day, you lose life itself. Do not worry about the time experience. Each new day a wave which has passed cannot be called back nor can an hour which has gone be returned. Your time will come by and by, there ar great things in store for you. Swift speedy time will bring new and interesting things this month, feathered with flying hours. Be careful ofer the years not to let it dissolve the beauty of thy brow. \n\nLucky Numbers: 43, 11, 22, 18, 41, 23",
+
+    "I see a great deal of happiness in store for you. Thousands of candles can be lighted from a single cangle and the life of the candle will not be shortened. Happiness never decreases by being shared. So, if an object you ardently pursue brings little happiness when gained, remember most of our pleasures come from unexpected sources. Share the good news when it arrives. For happiness is like a sunbeam which the least shadow shall intercept, while suffering is often as the rain of spring. Enjoy this very day! \n\nLucky Numbers: 24, 39, 21, 12, 31, 04",
+
+    "You've been wondering what to do with all those benefits you have or are about to receive. Just remember, as the branches of a tree return their sap to the root, from where it comes, as a river pours its streams to the sea, where its spring was supplied. The heart of the grateful person is delighted to return a benefit received. You are a kind and good person. You find confidence in the goodness of another. This is good proof of one's own goodness. Your heart may be pleased to know that there is many a good person to be found under a shabby hat. \n\nLucky Numbers: 30, 35, 10, 27, 05, 26",
+
+    "Now is the time to start that new project you have been contemplating. Your deliberation will pay off in the long run because doubt is the father of invention and the key to knowledge. This new found industry pays debts, while if you despair, you will only increase them. The time is right to get going. If you are to move the world, first you must move yourself. If you find yourself working too hard to achieve your goal, you may find basic truth. The great person with vigor would demand the rightness of things, timeliness of action, and propriety of method. In this way, power does not become sheer force. You will be surpirsed at what you will accomplish. \n\nLucky Numbers: 46, 17, 48, 20, 13, 11",
+
+    "You've been trying to figure out the best time to travel. You're wondering if a business trip or a vacation would be good. You may be wondering if to travel is a better thing than to arrive. It is not fit that everyone should travel. You change your sky, not your mind, when you cross the sea or land. A busy idleness possesses you; you seek a happy life, with ships, cars, and planes. The object of your search is present within you. If you travel for your holiday, remember a good holiday is one spent among people whose notions of time are more vague than your own. \n\nLucky Numbers: 39, 34, 28, 08, 02, 32",
+    
+    "You may be riding the winds of change. Things may at times seem to be out of touch. Soon they will come down to a better order. As the blessings of health and fortune have a beginning, so they must also find an end. Everything rises but to fall. To make sense of all this, you will find the answer is to be found in how well you relate to people. Looking at the big picture will help shape your long-range plans. If things are not always smooth remember that the person who could foresee affairs but three days in advance would be rich for thousands of years. \n\nLucky Numbers: 14, 17, 30, 34, 19, 47",
+    
+    "Round and round the ball will spin, till it draws your good luck in. Ah, I can foretell for you good luck in a month or two. \n\nThe crystal gazer has wonderful things in store for you. A dear one will return from a long trip and your whole life will be different. You have a patient disposition and your patience is about to be rewarded. Despair not I say for your days of despair will soon be over. Your calm spirit and your good sense will see you through all emergencies. You have many friends. They are loyal to you and are glad of an opportunity to be of service to you. \n\nLucky Numbers: 3, 26, 6, 44, 21, 12",
+    
+    "Standard setting directives you set must be followed but the important thing is self-reformation. The words of appointment for accomplishment are pleasing but the important thing is self-reflection. There is nothing you can do to gain complete satisfaction without self-reflection. Nor can there be compliance without self-reformation. Put loyalty and reliability first. If you have faults, do not fear self-improvement. \n\nLucky Numbers: 02, 25, 04, 10, 41, 03",
+    
+    "Tomorrow seems to be the busiest day of the week for you! A tall list of tasks weighs heavily on the mind. Quit procrastinating and do what you have been saying you will do, today. Relief and peacefulness awaits! \n\nLucky Numbers: 24, 38, 12, 09, 22, 30",
+
+    "You are a very virtuous person. you have chosen the path of righteousness and you will be very happy. You are not easily influenced. Your relatives have a great deal of respect for you. They know that you have a keen mind and an understanding nature. You have a good sense of values. You love a beautiful home and one of these days you will be in possession of one. \n\nLucky Numbers: 01, 19, 31, 06, 21, 18",
+
+    "You emerge from your recent fog and get down to business. Life becomes more serious and realistic. If a person takes no thought about what is distant, they will find sorrow near at hand. There is a need for good business judgement now. When things are going well it is all too easy to become over-confident and not give proper attention to one's duties. If this tendency is recognized early it may be corrected an no harm will be done. You will also find time after business is settled for friends and or lovers. It is easy to see, hard to foresee. \n\nLucky Numbers: 03, 13, 16, 22, 34, 32",
+
+    "A new turn of events will soon come about. A happy reunion with a loved one will make life all that you ever wanted it or dreamed it to be. You have a very trusting nature and are easily taken in by so-called friends. Do not be so anxious to do favors unto others, as there is one who is just waiting to take advantage of your good nature. \n\nLucky Numbers: 42, 33, 07, 31, 22, 27",
+    
+    "Do you like to lay in bed just to rest your sleepy head? That's OK for a day or two, but as a habit, 'twill never do. \n\nIf you don't want to have too many regrets in your life, get up early, get there first and success will come to you instead of the next individual. You have a very pleasant disposition and know how to make the most of it. Life for you hasn't been to easy, but I foresee a turn of events that will give you a great deal of happiness. When opportunity comes, grab it and don't let it slip through your fingers. \n\nLucky Numbers: 09, 15, 28, 24, 13, 45",
+    
+    "Yes, you've been up against it at times and fate has been at the bottom of it all. Now you can look ahead to better times. You will dream about sun and that will be the beginning of a bright future. You are a very sincere individual and like to be surrounded by your good friends. Your money troubles will soon disappear and you will be able to indulge in a few luxuries that you've been craving. You will be besiege by those less fortunate for monetary help and no one will go unaided. \n\nLucky Numbers: 20, 48, 15, 45, 02, 40",
+
+    "Oh, for the powers the gifts give us, to see ourselves as others see us. (Bobby Burns) \n\nWell my dear outspoken one, you are so quick to criticize others, did you ever stop to analyze yourself. You are a very sensitive person and a very critical one. you have a very sharp tongue which may cause unhappiness to others. You are of a generous disposition and knowingly would not hurt anyone. You have a keen mind. Try to improve it. Your best friends like you for your ready wit. Try to cultivate a red haired person. Therin lies a great deal of happiness for you. \n\nLucky Numbers: 26, 32, 05, 39, 36, 01",
+
+    "Oh, how you love to whistle and sing. Oh, how you love to dance and swing. Your future life with joy will ring with all the happiness it will bring. \n\nYou have a very sympathetic nature. You devote a great many hours to the welfare of others. you have a very find mind and if you cultivate it properly, you will be very successful. You are very fond of sports and love to dance. You have a graceful walk, and a determined step. you always walk as though you know exactly where you're going. People respect you for your determination. You will endure some hardship in the near future, but eventually everything will turn out for the best and unending happiness will be yours. \n\nLucky Numbers: 22, 44, 37, 38, 35, 20",
+	
+  	"When you pull a card and draw a heart, you and your beloved will never part. You are a strong believer in fate. You feel that you have no control over your destiny. Fortunately you are destined to be very happy indeed. You've had some trouble mostly caused by inconsideration of others. But fate will be kind to you and you can expect your life to run on a smoother pattern. you are somewhat irresponsible and that has caused you some hardships. You have a neat and tidy nature, and can't tolerate slovenliness around you. Since you demand this of yourself and others, you will always live in a tidy atmosphere. \n\nLucky Numbers: 36, 37, 14, 07, 19, 29",
+	
+  	"Unexpected wealth will arrive. Remember a person is richest who is content with the least, for content is the wealth of nature. Although you may look for riches, realize that wealth consists not in having great possessions, but in having few wants. When riches come, do not forget that a person's true wealth is the good he does in the world. So, when opportunity knocks, grab it. It is easy at any moment to resign the possession of great fortune, to acquire it is difficult and arduous. \n\nLucky Numbers: 15, 40, 04, 37, 23, 07",
+
+    "A wise old owl sat on an oak. The more he sat, the less he spoke. The less he spoke, the more he heard. Why can't you be like that wise old bird? \n\nYes, my friend, your greatest fault is that you talk too much. Learn to keep a secret. However, your other golden qualities make up for your talkativeness. Your anxiety to help others, and your consideration of other people's wishes has earned you many friend. \n\nLucky Numbers: 01, 10, 07, 09, 23, 02",
+
+    "Romance or great friendship you'll soon find adds great pleasure to your life. You may find that it is difficult to know at what moment this love begins, but less difficult to know that it has begun. Your love is eternal - the aspect may change, but not the essence. There is the same difference in you before and after you are in love, as there is in an unlighted lamp and one that is burning, The lamp was there and was a good lamp, but now it is shedding light too, and that is its real function. As the greatest pleasure of life is love. Remember when this love comes or if it has already arrived the heart is wiser than the intellect. \n\nLucky Numbers: 46, 21, 38, 23, 09, 10",
+
+    "If you subdue yourself and return to the practice of what is right. If one day you achieve self-control and return to what is right. The world will acknowledge you as a person at their best. Being the best must come from you yourself. One cannot acquire it from others. Look at nothing which is contrary to what is right. Listen to nothing contrary to what is right. Speak nothing contrary to what is right. And, do nothing contrary to what is right. you will then be a person at their best. \n\nLucky Numbers: 27, 05, 28, 18, 06, 30",
+	
+];
+
+var zoltar = zoltarOpen[parseInt(Math.random() * zoltarOpen.length)];
+
+var future = futures[parseInt(Math.random() * futures.length)];
+
+var sendFuture = new x_snc_slackerbot.Slacker().send_chat(current, "Trust Zoltar. He knows, sees, and tells ALL!\n\n" + zoltar + '\n\n' + future, true);

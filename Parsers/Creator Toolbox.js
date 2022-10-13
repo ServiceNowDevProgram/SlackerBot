f807af91e0c5d79ccab442daed6af5d79510c036
AashishAtrey <aatrey882@gmail.com>
2022-10-14 00:15:56 +0530

    Update Creator Toolbox.js

--- Parsers/Creator Toolbox.js
+++ Parsers/Creator Toolbox.js
@@ -1,10 +1,14 @@
 /*
 activation_example:!ctb
-regex:(!ctb)||(!creatortoolbox)||(!toolbox) 
+regex:(!ctb)|(!creatortoolbox)|(!toolbox) 
 flags:gmi
 */
 var sentence = current.text;
 
 
-var message = '<https://www.youtube.com/watch?v=fDvEqAQIHzQ&list=PL3rNcyAiDYK08_Xjuvu6IFEK8pqFT99WQ>';
+var sentence = current.text;
+
+
+var message = "The latest episode of Creator Toolbox " + '<https://www.youtube.com/watch?v=fDvEqAQIHzQ&list=PL3rNcyAiDYK08_Xjuvu6IFEK8pqFT99WQ>';
+
 var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, message, true);

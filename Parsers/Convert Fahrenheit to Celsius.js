75a0bca2ae22c57cf8e1fa882c3ba2f7ad1c1a15
wiz0floyd <wiz0floyd@gmail.com>
2022-10-17 16:05:36 -0400

    Fix units in F to C converter
    Fixes #173

--- Parsers/Convert Fahrenheit to Celsius.js
+++ Parsers/Convert Fahrenheit to Celsius.js
@@ -10,4 +10,4 @@ var numbertest = /-?\d{1,}\.?\d{0,}/;
 var numbermatch = numbertest.exec(match[0]);
 var ftoc = (parseFloat(numbermatch[0]) - 32) * 5/9;
 var originalnumber = parseFloat(numbermatch[0]).toFixed(2).toString().slice(-3) == '.00' ? parseFloat(numbermatch[0]).toFixed(2).toString().slice(0, -3) : parseFloat(numbermatch[0]).toFixed(2);
-var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, originalnumber + 'C is ' + ftoc.toFixed(2) + ' degrees in sane units (Celsius).');
+var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, originalnumber + '&deg;F is ' + ftoc.toFixed(2) + ' degrees in sane units (Celsius).');

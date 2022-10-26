7d7e89a720c34e9e632795524c61f1a6cf3bf74b
Techiadarshrock <adarshsingh0205@gmail.com>
2022-10-26 14:05:58 -0400

    more optimized calculation

--- Parsers/Convert Celsius to Fahrenheit.js
+++ Parsers/Convert Celsius to Fahrenheit.js
@@ -8,6 +8,6 @@ var regextest = /(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?c(?:elsius|el
 var match = regextest.exec(current.text);
 var numbertest = /-?\d{1,}\.?\d{0,}/;
 var numbermatch = numbertest.exec(match[0]);
-var ctof = (parseFloat(numbermatch[0]) * 9/5) + 32;
+var ctof = (parseFloat(numbermatch[0]) * (9/5)) + 32;
 var originalnumber = parseFloat(numbermatch[0]).toFixed(2).toString().slice(-3) == '.00' ? parseFloat(numbermatch[0]).toFixed(2).toString().slice(0, -3) : parseFloat(numbermatch[0]).toFixed(2);
 var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, originalnumber + '°C is ' + ctof.toFixed(2) + ' degrees in freedom units (Fahrenheit).');

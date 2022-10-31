bc187be95bbea0679fe04c4d7607a3810d75fc49
Ishaan S <50629150+ishaanshoor@users.noreply.github.com>
2022-11-01 04:30:08 +1100

    Create meetups.js

---
+++ Parsers/meetups.js
@@ -0,0 +1,12 @@
+/*
+activation_example:!meetups
+regex:^!meetups
+flags:gmi
+*/
+
+(function(){
+
+  var strMessage = 'Checkout `https://devlink.sn/meetups` for all upcoming ServiceNow Developer meetup events in and around your area.'
+  var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, strMessage, false);
+
+})();

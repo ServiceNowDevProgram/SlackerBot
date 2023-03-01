38546ddf74a606856a9950a1f4c07bdd69c226e4
Chris Helming <7746625+chelming@users.noreply.github.com>
2023-03-01 16:13:13 -0500

    Update Live Coding Happy Hour.js

--- Parsers/Live Coding Happy Hour.js
+++ Parsers/Live Coding Happy Hour.js
@@ -6,5 +6,5 @@ flags:gmi
 
 var t = current.text;
 
-var message = '<https://www.youtube.com/watch?v=EzE_JLPwCy8&list=PL3rNcyAiDYK3YiwkTj8OUSs_FRz-YSIw2&ab_channel=ServiceNowDevProgram|LCHH>';
+var message = '<https://www.youtube.com/playlist?list=PL3rNcyAiDYK3YiwkTj8OUSs_FRz-YSIw2|LCHH>';
 var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, message, false);

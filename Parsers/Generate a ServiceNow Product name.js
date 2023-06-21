adb205d78a82aa10c845f153eb3e55dfcd7952c0
Earl Duque <31702109+earlduque@users.noreply.github.com>
2023-06-21 10:26:00 -0700

    Update Generate a ServiceNow Product name.js

--- Parsers/Generate a ServiceNow Product name.js
+++ Parsers/Generate a ServiceNow Product name.js
@@ -15,7 +15,7 @@ var names = [
 
 var buzzwords = ["Blockchain", "AI", "IoT", "BigData", "Cloud", "Quantum", "VR", "AR", "Cybersecurity", "Analytics", "MachineLearning", "DeepLearning", "NLP", "Cryptocurrency", "Bitcoin", "Agile", "DevOps", "Containers", "Microservices", "Fintech", "Insurtech", "Edtech", "SaaS", "PaaS", "IaaS", "5G", "AR", "VR", "Autonomous", "Drones", "EdgeComputing", "Robotics", "SmartHome", "DataScience", "DataMining", "Chatbot", "Wearable", "GenAI", "Integration", "Admin", "Reports", "SEO", "UI", "UX", "GrowthHacking", "DataBreach", "DDoS", "Ransomware", "Firewall"];
 for (var i in buzzwords){
-  let buzzword = {
+  var buzzword = {
     text: buzzwords[i],
     order: 0,
     end: false,
@@ -26,7 +26,7 @@ for (var i in buzzwords){
 
 var suffices = ["Experience", "UI", "Framework", "Workspace", "Environment", "Platform", "Suite", "Engine", "System", "Network", "Cloud", "Hub", "Interface", "Kit", "Tool", "Pro", "Plus", "Max", "X", "Matrix", "Box", "Gate", "Base", "Dome", "Sphere", "Realm", "Vista", "Horizon", "Nexus", "Oasis", "Solutions", "Point", "Link", "Connect", "Verse", "Infinity", "Edge", "Prime", "Core", "Zone", "Lab", "Fusion", "Rise", "Boost", "Wave", "Dynamo", "Pulse", "Bolt", "Vault", "Rush", "Expanse", "Grid"];
 for (var j in suffices){
-  let suffix = {
+  var suffix = {
     text: suffices[j],
     order: 1,
     end: false,
@@ -37,13 +37,13 @@ for (var j in suffices){
 
 var enders = ["2.0", "Pro", "V2", "Enterprise", "Lite", "Premium", "Plus", "Advanced", "Deluxe", "Professional", "Ultimate", "Classic", "Standard", "Express", "Turbo", "Gold", "Silver", "Bronze", "V1", "V3", "V5", "V10", "3.0", "4.0", "5.0", "X", "XL", "XXL", "LE", "SE", "Ultra", "Mega", "Giga", "Tera", "Peta", "Max", "Prime", "Alpha", "Beta", "Gamma", "Omega", "NextGen", "Future", "2023", "2024", "2025"];
 
-names.sort((a, b) => a.order - b.order);
-
-let currentOrder = 0;
+var currentOrder = 0;
 var results = [];
 
-while () {
-  let filteredNames = names.filter(obj => obj.order === currentOrder);
+while (currentOrder < 2) {
+  var filteredNames = names.filter(function(obj) {
+    return obj.order === currentOrder;
+});
   if (filteredNames.length > 0){
     var item = filteredNames[Math.floor(Math.random()*filteredNames.length)];
     if (!item.nospace){
@@ -57,7 +57,7 @@ while () {
   }
 }
 
-if (Math.random() < 0.5) results.push(" " + enders[Math.floor(Math.random()*enders.length]);
+if (Math.random() < 0.5) results.push(" " + enders[Math.floor(Math.random()*enders.length)]);
 
 var message = "Introducing ServiceNow's newest product: " + result.join();
 var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, message, false);

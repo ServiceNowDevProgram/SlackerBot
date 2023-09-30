d8fa52a90a6f2669e6e2990e396e1c671225ee20
Earl Duque <31702109+earlduque@users.noreply.github.com>
2023-09-30 14:54:38 -0700

    cleaning up

--- Parsers/Utah_BASE_1685.js
+++
@@ -1,9 +0,0 @@
-/*
-activation_example: Please check the #utah channel
-regex: #utah
-flags: gmi
-order:200
-stop_processing:false
-*/
-
-new x_snc_slackerbot.Slacker().send_reaction(current, 'utah');

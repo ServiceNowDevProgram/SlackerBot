b64a8dbf0ad0d03ea4a5e50591b2233da34cd1f8
Earl Duque-HomePC <earl@earlduque.com>
2023-09-30 14:52:44 -0700

    merge conflicts

---
+++ Parsers/Utah_BASE_1685.js
@@ -0,0 +1,9 @@
+/*
+activation_example: Please check the #utah channel
+regex: #utah
+flags: gmi
+order:200
+stop_processing:false
+*/
+
+new x_snc_slackerbot.Slacker().send_reaction(current, 'utah');

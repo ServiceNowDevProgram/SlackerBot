636046c98e4c24c19771563305dceebb9315620b
djunaid <43455424+djunaid@users.noreply.github.com>
2023-10-01 16:37:54 -0600

    This fix the issue 200 !trout trigger slap whole phrase

--- Parsers/Slap someone with a trout.js
+++ Parsers/Slap someone with a trout.js
@@ -11,7 +11,7 @@ var troutFlag = (current.text.indexOf('!trout') > -1);
 var term = current.text.replace((troutFlag ? '!trout' : '!slap'), '').trim();
 
 //Determine message to send
-var msg = (term ? '<@'+current.user.user_id+'> slapped ' + term + ' with a large trout' : ':upside_down_face: ' + (troutFlag ? '!trout' : '!slap') + ' *something*');
+var msg = (term ? '<@'+current.user.user_id+'> slapped ' + term.split(' ')[0] + ' with a large trout' : ':upside_down_face: ' + (troutFlag ? '!trout' : '!slap') + ' *something*');
 
 //Send Slack message
 var sendIt = new x_snc_slackerbot.Slacker().send_chat(current, msg, false);

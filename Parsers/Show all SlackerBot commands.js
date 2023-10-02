a935022e7f3a37ccee9768a2e323dc34bfb78d69
Raunak Scarlet <raunakscarlet@gmail.com>
2023-10-02 22:50:11 +0530

    Update Show all SlackerBot commands.js
    added updated !help parser

--- Parsers/Show all SlackerBot commands.js
+++ Parsers/Show all SlackerBot commands.js
@@ -4,14 +4,16 @@ regex:!help
 flags:gmi
 */
 
+
+
 var parser = new GlideRecord('x_snc_slackerbot_parser');
 parser.addQuery('active', 'true');
 parser.orderBy('activation_example');
 parser.query();
 var helps = [];
 while(parser.next()){
-	if (parser.getValue('description').indexOf('Admin tool') == -1 || 
-		(parser.getValue('description').indexOf('Admin tool') > -1 && (current.channel == "GD51HTR46" || current.channel == "G9LAJG7G8" || current.channel == "G7M4AP6U8"))) {
+	if (parser.getValue('include_in_help')==1 && (parser.getValue('description').indexOf('Admin tool') == -1 || 
+		(parser.getValue('description').indexOf('Admin tool') > -1 && (current.channel == "GD51HTR46" || current.channel == "G9LAJG7G8" || current.channel == "G7M4AP6U8")))) {
 		helps.push('`' + (parser.getValue('activation_example') ? parser.getValue('activation_example') : '+:' + parser.getValue('emoji') + ':') + '` ' + parser.getValue('description'));
 	}
 }

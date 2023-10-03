3862b0f46471e657ab69b296663f968059657797
Ladirinia <50149432+Ladirinia@users.noreply.github.com>
2023-10-03 11:51:48 +0200

    Create Wshington triggers Washington react.js

---
+++ Parsers/Wshington triggers Washington react.js
@@ -0,0 +1,9 @@
+/*
+activation_example: Please check the washington channel
+regex:\bwashington\b
+flags:gmi
+order:200
+stop_processing:false
+*/
+
+new x_snc_slackerbot.Slacker().send_reaction(current,'washington');

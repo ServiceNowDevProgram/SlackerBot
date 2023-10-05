6d2718067fce9bd15dad1f70da795572b75dc9ea
eriemer1 <77909520+eriemer1@users.noreply.github.com>
2023-10-05 08:55:24 -0600

    Update Welcome react.js
    fixes to add actual bullets instead of letting markdown do it and "" where they were needed

--- Parsers/Welcome react.js
+++ Parsers/Welcome react.js
@@ -4,7 +4,8 @@ emoji:welcome
 
 var message = {
 	"type": "home",
-	"blocks": [{
+	"blocks": [
+		{
 			"type": "header",
 			"text": {
 				"type": "plain_text",
@@ -35,7 +36,7 @@ var message = {
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": "- Use Threads! #general gets busy so make sure to keep conversation about your question in the original thread. \n- Write a title that summarizes the specific problem. \n- Pretend you're talking to a busy colleague.\n -Spelling, grammar, and punctuation are important! \n- Bad: GlideRecord isn't working \n- Good: Why does str == 'value' evaluate to false when str is set to 'value' in my script? \n- Proof-read before posting! \n- Post the question and respond to feedback"
+				"text": " • Use Threads! #general gets busy so make sure to keep conversation about your question in the original thread. \n • Write a title that summarizes the specific problem. \n • Pretend you're talking to a busy colleague.\n • Spelling, grammar, and punctuation are important! \n • Bad: GlideRecord isn't working \n • Good: Why does str == 'value' evaluate to false when str is set to 'value' in my script? \n • Proof-read before posting! \n • Post the question and respond to feedback"
 			}
 		},
 		{
@@ -52,7 +53,7 @@ var message = {
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": "- Introduce the problem before you post any code. \n- Help others reproduce the problem. Don't just copy in your entire script. \n- Do not post images of code, data, error messages (just copy the text).\n -When posting code wrap it in 1x and 3x back-tick [`] syntax, or use the code editor \n- `your text` = Red highlighted single-line \n ```your text``` = Grey highlighted multi-line \n- Or use the </> code button on the text editor in slack."
+				"text": " • Introduce the problem before you post any code. \n • Help others reproduce the problem. Don't just copy in your entire script. \n- Do not post images of code, data, error messages (just copy the text).\n • When posting code wrap it in 1x and 3x back-tick [`] syntax, or use the code editor \n ````your text` = Red highlighted single-line``` \n ``````your text`````` ```= Grey highlighted multi-line``` \n • Or use the </> code button on the text editor in slack."
 			}
 		},
 		{
@@ -69,7 +70,7 @@ var message = {
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": "If someone successfully helped you (or ever just help you get closer to the answer) tag them with a ++ in the thread. eg. <@' + current.user.user_id + '> ++ will grant that user some community points to show how helpful they were."
+				"text": "If someone successfully helped you (or ever just help you get closer to the answer) tag them with a ++ in the thread. eg. <@" + current.user.user_id + "> ++ will grant that user some community points to show how helpful they were."
 			}
 		},
 		{

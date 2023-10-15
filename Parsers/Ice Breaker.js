5cbde1062853dad0a9e77166d26ec8488566541f
David.Marcial <davidmarcial@Davids-MacBook-Pro-2.local>
2023-10-15 15:21:31 -0600

    Added the first couple of ice breakers to test

--- Parsers/Ice Breaker.js
+++ Parsers/Ice Breaker.js
@@ -3,3 +3,22 @@ activation_example:!ice breaker
 regex:(!ice breaker)
 flag:gmi
 */
+
+var ice_breakers = [
+    {
+        prompt: "How many siblings do you have?"
+    }, {
+        prompt: "What's your favorite movie?"
+    }, {
+        prompt: "What do you do for fun?" 
+    }, {
+        prompt: "Would you rather fight 100 duck-sized horses or 1 horse-sized duck?" 
+    }, {
+        prompt: "What's your name, and is there a story behind it?"  
+    } 
+];
+
+var random = Math.floor(Math.random() * ice_breakers.length);
+
+//Send the ice breaker prompt to Slack
+new x_snc_slackerbot.Slacker().send_chat(current, ice_breakers[random].prompt, false);

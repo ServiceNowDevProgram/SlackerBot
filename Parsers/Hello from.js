4763b6fc3a2317e87186339adecc2d134808e260
almajam <148045187+almajam@users.noreply.github.com>
2023-10-16 15:42:31 +0200

    Create Hello from.js
    If you say "Hello from" ot "hellofrom" then bot replies a greeting from a random place, telling the local time as well. Feel free to add more cities and timezones to responses array.

---
+++ Parsers/Hello from.js
@@ -0,0 +1,21 @@
+/*
+activation_example:hellofrom
+regex:(hellofrom)|(hello\sfrom) 
+flags:gmi
+*/
+
+var responses = [
+    ["IST","Delhi, India"],
+    ["PST","San Diego, USA"],
+    ["GMT","London, UK"],
+    ["CET","Budapest, Hungary"]
+];
+var randNumber=parseInt(Math.random() * responses.length);
+var currentDateTime=new GlideDateTime();
+var strConvertedDateTime= new GlideScheduleDateTime(currentDateTime).convertTimeZone("UTC", responses[randNumber][0]); 
+var gdtConvertedDateTime = new GlideDateTime(strConvertedDateTime); 
+
+var response="Hello from " + responses[randNumber][1] + ". Our current time is " + gdtConvertedDateTime;
+//gs.info(response);
+
+var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, response, true);

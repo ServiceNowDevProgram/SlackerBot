8250b8b0514e8789f1a9408c7188ca24825e06c6
David.Marcial <davidmarcial@Davids-MacBook-Pro-2.local>
2023-10-18 17:58:35 -0600

    Added the ice breaker reaction

--- Parsers/Ice Breaker.js
+++ Parsers/Ice Breaker.js
@@ -64,3 +64,16 @@ var random = Math.floor(Math.random() * ice_breakers.length);
 
 //Send the ice breaker prompt to Slack
 new x_snc_slackerbot.Slacker().send_chat(current, ice_breakers[random].prompt, false);
+
+//This should add the cold_face emoji and spell out ice breaker with the laser letters
+new x_snc_slackerbot.Slacker().send_reaction(current, 'cold_face');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-i');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-c');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-e');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-b');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-r');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-e');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-a');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-k');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-e');
+new x_snc_slackerbot.Slacker().send_reaction(current, 'laser-r');

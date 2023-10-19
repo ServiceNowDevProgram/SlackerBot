a77bb187fe623f2d58958c3313cbe9146bed1e34
Raunak Scarlet <raunakscarlet@gmail.com>
2023-10-19 19:27:30 +0530

    Create Release Washington.js
    whenever in chat washington word appears, bot will react to the washington emoji

---
+++ Parsers/Release Washington.js
@@ -0,0 +1,10 @@
+/*
+activation_example:washington
+regex:(^|[^:\/\w])washington(?![\/\w])
+flags:gmi
+order:200
+stop_processing:false
+*/
+
+
+new x_snc_slackerbot.Slacker().send_reaction(current,'washington');

275fbb8a0c7026575e563c0f476852ad2cdb223d
Raunak Scarlet <raunakscarlet@gmail.com>
2023-10-24 22:48:56 +0530

    Create text to SponGeBoB case.js
    The task is to convert text into spongebob case which means random capitalization of characters appeared in the text
    
    if the user use the command !spongebob in the text, then !spongebob parser will be activated 
    
    for example: 
    !spongebob what is name YOUr name
    
    possible output :
     whAt iS nAME yoUr naME

---
+++ Parsers/text to SponGeBoB case.js
@@ -0,0 +1,12 @@
+
+/*
+activation_example:!spongebob your sentence
+regex:!spongebob
+flags:gmi
+*/
+
+var text =current.text.replace(/!spongebob/gmi, "").trim();
+var newText = text.split('').map(function(e) {
+    return Math.floor(Math.random() * 2) ? e.toUpperCase() : e.toLowerCase();
+}).join('').replaceAll('I','i').replaceAll('l','L');
+new x_snc_slackerbot.Slacker().send_chat(current, newText);

b4e4c83c991f27628326df7749c350f4782dc3c7
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2023-10-28 17:51:50 +0530

    Create Downdetector.js

---
+++ Parsers/Downdetector.js
@@ -0,0 +1,11 @@
+/*
+activation_example:!clap your sentence
+regex:!clap
+flags:gmi
+*/
+var sentence = current.text.replace(/!clap/gmi, "").trim().toUpperCase();
+if (sentence == '') {
+	var send_confusion = new x_snc_slackerbot.Slacker().send_chat(current.channel, ':upside_down_face: gimme something to clap!', false, '', current.thread_ts);
+} else {
+	new x_snc_slackerbot.Slacker().send_chat(current, sentence.split(' ').join(' :clap: '), current.thread_ts);
+}

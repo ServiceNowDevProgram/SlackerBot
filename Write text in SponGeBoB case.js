3cb3e9bebae9a4a640c011c0b2e72ffba6d98cab
Astrid Sapphire <59789839+SapphicFire@users.noreply.github.com>
2023-10-29 20:47:35 +1000

    fix: Remove whitespace
    Drop whitespace that was blocking this parser from being read

--- Write text in SponGeBoB case.js
+++ Write text in SponGeBoB case.js
@@ -1,11 +1,10 @@
-
 /*
 activation_example:!spongebob your sentence
 regex:!spongebob
 flags:gmi
 */
 
-var text =current.text.replace(/!spongebob/gmi, "").trim();
+var text = current.text.replace(/!spongebob/gmi, "").trim();
 var newText = text.split('').map(function(e) {
     return Math.floor(Math.random() * 2) ? e.toUpperCase() : e.toLowerCase();
 }).join('').replaceAll('I','i').replaceAll('l','L');

2e88527e1a817fab567fc747fd94da3c527bfb41
Astrid Sapphire <59789839+SapphicFire@users.noreply.github.com>
2024-04-05 23:43:55 +1000

    misc: update messaging

--- Parsers/Admin tool - Verify.js
+++ Parsers/Admin tool - Verify.js
@@ -19,7 +19,7 @@ if (current.channel == "GD51HTR46" || current.channel == "G9LAJG7G8" || current.
     if(invocation.length == 0){
       verification_status = !(grUser.getValue('verified') == 1 ? true : false); // Get verified and toggle
       grUser.setValue('verified',verification_status);
-      message_body += 'Updated <@' + user_id + '>\'s verification status. They are now ' + (verification_status ? 'verified.' : 'unverified. To add a user-visible note, add text after `!verify-admin @User `.');
+      message_body += 'Updated <@' + user_id + '>\'s verification status. They are now ' + (verification_status ? 'verified.' : 'unverified. To add a user-visible note, add text after `!verify-admin @User `. Example: `!verify-admin @Astrid Is upside down`');
     } else {
       grUser.setValue('user_info', invocation);
       grUser.update();

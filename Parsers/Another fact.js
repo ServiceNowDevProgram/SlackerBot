f0e17ce27c5d7384710386b0cfb34d19b8e58a75
eriemer1 <77909520+eriemer1@users.noreply.github.com>
2024-09-30 13:30:19 -0600

    Update Another fact.js

--- Parsers/Another fact.js
+++ Parsers/Another fact.js
@@ -4,10 +4,12 @@ regex:fact
 flags:gmi
 */
 
-var fact = new sn_ws.RESTMessageV2();
-fact.setEndpoint('https://uselessfacts.jsph.pl/api/v2/facts/random/');
-fact.setHttpMethod("GET");
-var chatResponse = fact.execute();
-var chatResponseBody = JSON.parse(chatResponse.getBody());
+if (Math.floor(Math.random() * 4) == 0) {
+    var fact = new sn_ws.RESTMessageV2();
+    fact.setEndpoint('https://uselessfacts.jsph.pl/api/v2/facts/random/');
+    fact.setHttpMethod("GET");
+    var chatResponse = fact.execute();
+    var chatResponseBody = JSON.parse(chatResponse.getBody());
 
-new x_snc_slackerbot.Slacker().send_chat(current, 'Another fact is: " + chatResponseBody.text, false);
+    new x_snc_slackerbot.Slacker().send_chat(current, 'Another fact is: " + chatResponseBody.text, false);
+    }

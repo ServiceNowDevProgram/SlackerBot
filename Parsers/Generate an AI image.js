143d0f83be32155d93994e5881790e6d84a5d93e
Ronald Karim <ronald@karim.live>
2024-10-06 02:53:06 +0100

    Update !dalle parser to include error response
    On Status code not 200,
    respond to the user informing of failure and log error.
    Property may be created to disable error logging.
    
    On Status code of 200,
    continue to Upload to Cloudiary as per previous functionality,
    additional error response if cloudiary url nonexistant.

--- Parsers/Generate an AI image.js
+++ Parsers/Generate an AI image.js
@@ -12,35 +12,49 @@ dalleReq.setRequestHeader('Content-Type', "application/json");
 dalleReq.setRequestHeader('User-Agent', "ServiceNow");
 dalleReq.setRequestHeader("Accept", "*/*");
 var body = {
-	"prompt": prompt,
-	"n": 1,
-	"size": "1024x1024",
-	quality: "standard",
-	model: "dall-e-3",
-	"response_format": "url",
-	"user": current.user.name.toString()
+    "prompt": prompt,
+    "n": 1,
+    "size": "1024x1024",
+    quality: "standard",
+    model: "dall-e-3",
+    "response_format": "url",
+    "user": current.user.name.toString()
 };
 dalleReq.setRequestBody(JSON.stringify(body));
 var dalleResponse = dalleReq.execute();
+var dalleResponseStatusCode = dalleResponse.getStatusCode();
 var dalleResponseBody = JSON.parse(dalleResponse.getBody());
+var failureMessage = "Image Generation failed. If this is your first error please try again, otherwise contact an admin";
 
-//Cloudinary
-var cloudinary = new sn_ws.RESTMessageV2();
-cloudinary.setEndpoint('https://api.cloudinary.com/v1_1/dxllc568e/image/upload');
-cloudinary.setHttpMethod('POST');
-cloudinary.setRequestHeader('Content-Type', "application/json");
-cloudinary.setRequestHeader('User-Agent', "ServiceNow");
-cloudinary.setRequestHeader("Accept", "*/*");
-var cloudinaryBody = {
-	"file": dalleResponseBody.data[0].url,
-	"upload_preset": gs.getProperty("cloudinary_upload_preset"), 
-	"api_key": gs.getProperty("cloudinary_api_key"),
-	"timestamp": Math.floor(new Date().getTime() / 1000),
-};
+if (dalleResponseStatusCode != 200) {
+    new x_snc_slackerbot.Slacker().send_chat(current, failureMessage);
+    if (gs.getProperty("generateaiimage.debug.enable", "true") == "true") {
+        gs.error("Error: " + dalleResponseStatusCode + " \n" + dalleResponseBody.error.message + "\n" + "Prompt: " + prompt);
+    }
 
-cloudinary.setRequestBody(JSON.stringify(cloudinaryBody));
+} else {
+    //Cloudinary
+    var cloudinary = new sn_ws.RESTMessageV2();
+    cloudinary.setEndpoint('https://api.cloudinary.com/v1_1/dxllc568e/image/upload');
+    cloudinary.setHttpMethod('POST');
+    cloudinary.setRequestHeader('Content-Type', "application/json");
+    cloudinary.setRequestHeader('User-Agent', "ServiceNow");
+    cloudinary.setRequestHeader("Accept", "*/*");
+    var cloudinaryBody = {
+        "file": dalleResponseBody.data[0].url,
+        "upload_preset": gs.getProperty("cloudinary_upload_preset"),
+        "api_key": gs.getProperty("cloudinary_api_key"),
+        "timestamp": Math.floor(new Date().getTime() / 1000),
+    };
 
-var cloudinaryResponse = cloudinary.execute();
-var cloudinaryResponseBody = JSON.parse(cloudinaryResponse.getBody());
-new x_snc_slackerbot.Slacker().send_chat(current, "<" + cloudinaryResponseBody.url + "|" + prompt + ">\n");
-//new x_snc_slackerbot.Slacker().send_chat(current, "<" + cloudinaryResponseBody.url + "|" + prompt + "> (<" + "https://collection.cloudinary.com/dxllc568e/86896fd03420bdbdb47adcc037086bdf" + "|" + "gallery>)");
+    cloudinary.setRequestBody(JSON.stringify(cloudinaryBody));
+
+    var cloudinaryResponse = cloudinary.execute();
+    var cloudinaryResponseBody = JSON.parse(cloudinaryResponse.getBody());
+
+    if (cloudinaryResponseBody.url) {
+        new x_snc_slackerbot.Slacker().send_chat(current, "<" + cloudinaryResponseBody.url + "|" + prompt + ">\n");
+    } else {
+        new x_snc_slackerbot.Slacker().send_chat(current, failureMessage);
+    }
+}

1c1b4dd3a331fd8b583c2d8fd3a254e6e53ef565
Kyle Burns <kylburns89@gmail.com>
2024-10-06 20:44:50 -0500

    Update Convert Celsius to Fahrenheit.js
    Update C to F temperature parser to return all temperatures when there is more than 1 in the input string.
    
    Update parser for ECMAScript 2021 compatibility.

--- Parsers/Convert Celsius to Fahrenheit.js
+++ Parsers/Convert Celsius to Fahrenheit.js
@@ -4,23 +4,18 @@ regex:(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?c(?:elsius|elcius)?\b
 flags:gmi
 */
 
-var regextest = /(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?c(?:elsius|elcius)?\b/gmi;
-var numbertest = /-?\d{1,}\.?\d{0,}/;
-var conversions = [];
+const regexTest = /(-?\d{1,3}(?:\.\d{1,2})?)°?\s?(?:degrees?)?\s?c(?:elsius)?\b/gi;
+const celsiusToFahrenheit = c => ((c * 9 / 5) + 32).toFixed(2);
+const formatNumber = num => Number(num).toFixed(2).replace(/\.00$/, '');
+const conversions = [];
 
-var match;
-while ((match = regextest.exec(current.text)) !== null) {
-    var numbermatch = numbertest.exec(match[0]);
-    var celsius = parseFloat(numbermatch[0]);
-    var fahrenheit = (celsius * 9/5) + 32;
-    
-    var originalNumber = celsius.toFixed(2).toString().slice(-3) == '.00' ? 
-        celsius.toFixed(0) : celsius.toFixed(2);
-    
-    conversions.push(originalNumber + '°C is ' + fahrenheit.toFixed(2) + ' degrees in freedom units (Fahrenheit).');
-  
-    current.text = current.text.replace(match[0], fahrenheit.toFixed(2) + '°F');
-}
+current.text = current.text.replace(regexTest, (match, c) => {
+  const fahrenheit = celsiusToFahrenheit(c);
+  const formattedC = formatNumber(c);
+  conversions.push(`${formattedC}°C is ${fahrenheit} degrees in freedom units (Fahrenheit).`);
+  return `${fahrenheit}°F`;
+});
 
-var conversionMessage = conversions.join('\n');
-var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, conversionMessage);
+const conversionMessage = conversions.join('\n');
+
+new x_snc_slackerbot.Slacker().send_chat(current, conversionMessage);

9da265729f79cabbbdf9282fbdb2c18a42132e67
Adeelah Mir <97887471+ariyadmir@users.noreply.github.com>
2024-10-11 17:22:55 +0530

    Update Translator.js
    The API from LibreTranslate was encountering connection issues, so I updated the code to use a different translation service (My Memory)

--- Parsers/Translator.js
+++ Parsers/Translator.js
@@ -1,54 +1,59 @@
 /*
 activation_example:!translate Hello world to Spanish
-regex:!translate\s+(.+)\s+to\s+(\w+)
+regex:!translate\s+(.+?)\s+(?:to|into|in)\s+(\w+)$
 flags:gi
-order:500
-stop_processing:false
 */
 
-var regex = /!translate\s+(.+)\s+to\s+(\w+)/gi;
+var regex = /!translate\s+(.+?)\s+(?:to|into|in)\s+(\w+)$/gi;
 var match = regex.exec(current.text);
+
 if (match && match[1] && match[2]) {
     var phrase = match[1].trim();
     var language = match[2].trim().toLowerCase();
+    
     var languageMap = {
-        'spanish': 'es',
+        'afrikaans': 'af',
+        'arabic': 'ar',
+        'chinese': 'zh',
+        'dutch': 'nl',
         'french': 'fr',
         'german': 'de',
+        'hindi': 'hi',
         'italian': 'it',
-        // Add more languages as needed
+        'japanese': 'jpn',
+        'korean': 'ko',
+        'malay': 'my',
+        'pakistani': 'ur',
+        'portuguese': 'pt',
+        'spanish': 'es',
+        'turkish': 'tr'
     };
 
     var targetLanguageCode = languageMap[language];
+    
     if (!targetLanguageCode) {
         new x_snc_slackerbot.Slacker().send_chat(current, 'Sorry, I do not support the language "' + language + '".', false);
         return;
     }
 
-    var apiUrl = 'https://libretranslate.de/translate';
+    var apiUrl = 'https://api.mymemory.translated.net/get';
     var requestBody = {
         'q': phrase,
-        'source': 'en',
-        'target': targetLanguageCode,
-        'format': 'text'
+        'langpair': 'en|' + targetLanguageCode
     };
 
-    // Make an API call to the translation service
     var restMessage = new sn_ws.RESTMessageV2();
-    restMessage.setHttpMethod('POST');
-    restMessage.setEndpoint(apiUrl);
-    restMessage.setRequestHeader('Content-Type', 'application/json');
-    restMessage.setRequestHeader('Accept', 'application/json');
-    restMessage.setRequestBody(JSON.stringify(requestBody));
+    restMessage.setHttpMethod('GET');
+    restMessage.setEndpoint(apiUrl + '?q=' + encodeURIComponent(phrase) + '&langpair=en|' + targetLanguageCode);
     
     try {
         var httpResponse = restMessage.execute();
         var httpResponseStatus = httpResponse.getStatusCode();
-        
+
         if (httpResponseStatus === 200) {
             var translationData = JSON.parse(httpResponse.getBody());
-            var translatedText = translationData.translatedText;
-            var responseMessage = 'The translation of "' + phrase + '" to ' + language + ' is: ' + translatedText;
+            var translatedText = translationData.responseData.translatedText;
+            var responseMessage = phrase + 'translated into ' + language + ' is:\n\n ' + translatedText;
             new x_snc_slackerbot.Slacker().send_chat(current, responseMessage, false);
         } else {
             new x_snc_slackerbot.Slacker().send_chat(current, 'Sorry, I could not translate the phrase "' + phrase + '" to ' + language + '.', false);
@@ -57,5 +62,6 @@ if (match && match[1] && match[2]) {
         new x_snc_slackerbot.Slacker().send_chat(current, 'There was an error with the translation service. Please try again later.', false);
     }
 } else {
-    new x_snc_slackerbot.Slacker().send_chat(current, 'Please use the format `!translate [phrase] to [language]` to translate text.', false);
+    new x_snc_slackerbot.Slacker().send_chat(current, 'Please use the format `!translate [phrase] to/into/in [language]` to translate text.', false);
 }
+

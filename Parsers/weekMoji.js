ac47ccdb5cb77476041e9bef8239eca81bd52e69
prynacshrma <148194858+prynacshrma@users.noreply.github.com>
2024-10-28 19:16:50 +0530

    Changed emojis  (#449)
    * Create emojis_for_weekdays
    
    Added slackerbot script for weekly emoji reactions
    
    * Update emojis_for_weekdays
    
    * Update emojis_for_weekdays
    
    * Create weekMoji.js
    
    Script to provide emoji based on current day of the week.
    
    * Delete emojis_for_weekdays
    
    Deleted unintended inclusion
    
    * Update weekMoji.js
    
    Changed emojis as per global slack emojis available
    
    * Update weekMoji.js
    
    Updated Header as suggested

---
+++ Parsers/weekMoji.js
@@ -0,0 +1,21 @@
+/*
+emoji: calendar
+*/
+
+new x_snc_slackerbot.Slacker().send_reaction(current, weeklyMoji());
+
+function weeklyMoji() {
+    var weekEmojis = {
+        0: 'person_in_lotus_position',     // Sunday - calm and relaxation
+        1: 'coffee',      // Monday - getting over weekend with coffee
+        2: 'chart_with_upwards_trend',      // Tuesday - Moving forward through week
+        3: 'climbing',   // Wednesday - Work and only work
+        4: 'rocket', // Thursday - Powering through tasks and finishing them
+        5: 'beers',      // Friday - Chilling and enjoyment
+        6: 'beach_with_umbrella'     // Saturday - Rest; friends and faily time
+    };
+
+    var day = new GlideDateTime().getDayOfWeekUTC(); // Get the current day of the week (0 = Sunday, 6 = Saturday)
+    return weekEmojis[day];
+}
+//Added script for weekly emoji reactions

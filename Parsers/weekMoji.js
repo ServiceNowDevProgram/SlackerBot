f408919961907e11edf66d27f43f7cb51c639d61
prynacshrma <148194858+prynacshrma@users.noreply.github.com>
2024-10-28 12:11:21 +0530

    Update weekMoji.js
    Updated Header as suggested

--- Parsers/weekMoji.js
+++ Parsers/weekMoji.js
@@ -1,5 +1,5 @@
 /*
-emoji: saturday-chill
+emoji: calendar
 */
 
 new x_snc_slackerbot.Slacker().send_reaction(current, weeklyMoji());
@@ -9,9 +9,9 @@ function weeklyMoji() {
         0: 'person_in_lotus_position',     // Sunday - calm and relaxation
         1: 'coffee',      // Monday - getting over weekend with coffee
         2: 'chart_with_upwards_trend',      // Tuesday - Moving forward through week
-        3: 'climbing',   // Wednesday - Working only
+        3: 'climbing',   // Wednesday - Work and only work
         4: 'rocket', // Thursday - Powering through tasks and finishing them
-        5: 'beers',      // Friday - CHilling and enjoyment
+        5: 'beers',      // Friday - Chilling and enjoyment
         6: 'beach_with_umbrella'     // Saturday - Rest; friends and faily time
     };
 

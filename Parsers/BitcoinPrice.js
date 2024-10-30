3882416aa0a4f1d5035c8d727ad220674811a441
Chris Helming <7746625+chelming@users.noreply.github.com>
2024-10-30 09:08:51 -0400

    Update BitcoinPrice.js

--- Parsers/BitcoinPrice.js
+++ Parsers/BitcoinPrice.js
@@ -1,20 +1,18 @@
 /*
 activation_example:!bitcoinprice
-regex:!bitcoinprice
+regex:!bitcoinprice\b
 flags:gmi
 */
 
 var input = current.text.trim(); 
-if (/!bitcoinprice/i.test(input)) {  
-    var quote = new sn_ws.RESTMessageV2();
-    quote.setEndpoint('https://api.coindesk.com/v1/bpi/currentprice.json');
-    quote.setHttpMethod('GET');
+var quote = new sn_ws.RESTMessageV2();
+quote.setEndpoint('https://api.coindesk.com/v1/bpi/currentprice.json');
+quote.setHttpMethod('GET');
 
-    var chatResponse = quote.execute();
-    var chatResponseBody = JSON.parse(chatResponse.getBody());
+var chatResponse = quote.execute();
+var chatResponseBody = JSON.parse(chatResponse.getBody());
 
-    if (chatResponseBody && chatResponseBody.length > 0) {
-        var bitcoinpriceFormatted = chatResponseBody.bpi.USD.rate;
-        new x_snc_slackerbot.Slacker().send_chat(current, 'The current price of one bitcoin is: ' + bitcoinpriceFormatted + '$', false);
-    }
+if (chatResponseBody && chatResponseBody.bpi && chatResponseBody.bpi.USD && chatResponseBody.bpi.USD.rate) {
+    var bitcoinpriceFormatted = chatResponseBody.bpi.USD.rate;
+    new x_snc_slackerbot.Slacker().send_chat(current, 'The current price of one bitcoin is: ' + bitcoinpriceFormatted + '$', false);
 }

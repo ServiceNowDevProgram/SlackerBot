05336be29787b51390d57fb42e13868bec9db457
Pradeep567iaf <60265769+Pradeep567iaf@users.noreply.github.com>
2024-11-04 18:09:12 +0530

    Update Binary To Decimal Converter.js
    Resolve the Issue raised 
    1)  Please remove this capitalisation, as this adds confusion when compared to all other parsers. Additionally, it is not clear that this parser converts from binary to decimal. It might be worthwhile making it something like !bin2dec
    
    2) It's not clear from other details about this parser, that this will not support multiple bytes (i.e. 00110101 11111111) or breaks in the binary
    
    3) This does not use unordered lists so does not use slack markdown
    
    Now, this parser will accept the binary code with spaces and give output as a decimal value. Change the activation  to  !bin2dec <binary code> and also remove the slack markdown and show output as normal text.

--- Parsers/Binary To Decimal Converter.js
+++ Parsers/Binary To Decimal Converter.js
@@ -1,47 +1,52 @@
 /*
 activation_example:!converttoDecimal <binary_code>
-regex:^!converttoDecimal 
+regex:!bin2dec
 flags:i
 */
 
-// get the user input in format !converttoDecimal binary_code for example - !converttoDecimal 1011
-var input = current.text.trim(); 
-
+// get the user input in format !bin2dec binary_code for example - !bin2dec 00110101 11111111
+var input = current.text.trim();
 
 // Extract the Binary expression from the input
-var binaryCheckregx = /^[01]+$/; // regular expresion for valid binary code
-var match = input.match(/!converttoDecimal (\d+)/); // check input format 
+
+var binaryCheckregx = /^[01\s]+$/; // Regular expression to check if input contains only binary characters and spaces
+var commandRegex = /^!bin2dec ([01\s]+)$/; // Extract the binary expression, including spaces between bytes.
+var match = input.match(commandRegex); // Updated commandRegex to include spaces between binary code
 var expression = match ? match[1] : "";
 
 
 
 function evaluateBinaryExpression(binary_code) {
-    binary_code = binary_code.replace(/\s+/g, '');  // Remove whitespace
-	// execute the api to get the equivalent decimal value of binary
+    // Remove whitespace
+    binary_code = binary_code.replace(/\s+/g, '');
+    // execute the api to get the equivalent decimal value of binary
     var converter = new sn_ws.RESTMessageV2();
     var apiurl = 'https://networkcalc.com/api/binary/' + binary_code;
     converter.setEndpoint(apiurl); // set endpoint url
     converter.setHttpMethod('GET'); // GET method  
-    var response = converter.execute(); 
-    var result = JSON.parse(response.getBody()); // parse the response object 
-    return result; 
+    var response = converter.execute();
+    var result = JSON.parse(response.getBody()); // parse the response object
+    return result;
 }
 
-try{
-	// check the valid binary code in if statement
-	if(binaryCheckregx.test(expression)){
-	var result = evaluateBinaryExpression(expression); // call to evaluateBinaryExpression
-    var DecimalresultSlackMessage = "* Binary to Decimal Conversion :*\n" + "• *Binary value*: " + expression + "\n"  + "• *Decimal value*: " + result.converted + "\n";  // slack markdown message format
-    new x_snc_slackerbot.Slacker().send_chat(current,  DecimalresultSlackMessage , false); // display the output to user
-	}
-	else{
-    new x_snc_slackerbot.Slacker().send_chat(current, "Oops! I couldn't understand that. Please provide a valid Binary code.", false);
-	}
-}
-catch(error){
-	new x_snc_slackerbot.Slacker().send_chat(current, "Oops! Facing Issue while converting Binary to Decimal with error" + error, false); //handling exception in try block
-}
+try {
+    // check the valid binary code in if statement
+    if (binaryCheckregx.test(expression)) {
+        var result = evaluateBinaryExpression(expression); // call to evaluateBinaryExpression
 
+        var DecimalresultSlackMessage = "Decimal Value: " + result.converted;
+        new x_snc_slackerbot.Slacker().send_chat(current, DecimalresultSlackMessage , false); // display the output to user
+
+
+    } else {
+        new x_snc_slackerbot.Slacker().send_chat(current, "Oops! I couldn't understand that. Please provide a valid Binary code.", false);
+
+    }
+} catch (error) {
+
+    new x_snc_slackerbot.Slacker().send_chat(current, "Oops! Facing Issue while converting Binary to Decimal with error" + error, false); //handling exception in try block
+}
+ 
 
 
 

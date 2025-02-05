c8ad48a63b67bae20bf23d74ca0dfc33bab8d672
Chris Helming <7746625+chelming@users.noreply.github.com>
2025-02-05 13:50:01 -0500

    deactivate Convert Fahrenheit to Celsius.js

--- Parsers/Convert Fahrenheit to Celsius.js
+++ Parsers/Convert Fahrenheit to Celsius.js
@@ -3,7 +3,7 @@ activation_example:30f or 30 degrees Fahrenheit
 regex:(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?f(?:ahrenheit)?\b
 flags:gmi
 */
-
+/*
 const regexTest = /(-?\d{1,3}(?:\.\d{1,2})?)°?\s?(?:degrees?)?\s?f(?:ahrenheit)?\b/gi;
 const fahrenheitToCelsius = f => ((f - 32) * 5 / 9).toFixed(2);
 const formatNumber = num => Number(num).toFixed(2).replace(/\.00$/, '');
@@ -19,3 +19,4 @@ while ((match = regexTest.exec(current.text)) !== null) {
 const conversionMessage = conversions.join('\n');
 
 new x_snc_slackerbot.Slacker().send_chat(current, conversionMessage);
+*/

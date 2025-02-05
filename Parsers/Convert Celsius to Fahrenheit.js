0a77e97c8072ae76e1d593131c2f3e7ae7685dd4
Chris Helming <7746625+chelming@users.noreply.github.com>
2025-02-05 13:55:51 -0500

    Update Convert Celsius to Fahrenheit.js
    combine the conversions

--- Parsers/Convert Celsius to Fahrenheit.js
+++ Parsers/Convert Celsius to Fahrenheit.js
@@ -1,20 +1,31 @@
 /*
 activation_example:30c or 30 degrees celsius
-regex:(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?c(?:elsius|elcius)?\b
+regex:(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?(?:c(?:elsius)?|f(?:ahrenheit)?)\b
 flags:gmi
 */
 
-const regexTest = /(-?\d{1,3}(?:\.\d{1,2})?)°?\s?(?:degrees?)?\s?c(?:elsius)?\b/gi;
-const celsiusToFahrenheit = c => ((c * 9 / 5) + 32).toFixed(2);
 const formatNumber = num => Number(num).toFixed(2).replace(/\.00$/, '');
 const conversions = [];
 
-let match;
-while ((match = regexTest.exec(current.text)) !== null) {
-    const celsius = parseFloat(match);
+// convert C to F
+const celsiusRegex = /(-?\d{1,3}(?:\.\d{1,2})?)°?\s?(?:degrees?)?\s?c(?:elsius)?\b/gi;
+const celsiusToFahrenheit = c => ((c * 9 / 5) + 32).toFixed(2);
+let cMatch;
+while ((cMatch = celsiusRegex.exec(current.text)) !== null) {
+    const celsius = parseFloat(cMatch);
     const fahrenheit = celsiusToFahrenheit(celsius);
-    conversions.push(`${formatNumber(celsius)}°C is ${formatNumber(fahrenheit)} degrees in sane units (Fahrenheit).`);
+    conversions.push(`${formatNumber(celsius)}°C is ${formatNumber(fahrenheit)} degrees in freedom units (Fahrenheit).`);
 }
+
+// convert F to C
+const fahrenheitRegex = /(-?\d{1,3}(?:\.\d{1,2})?)°?\s?(?:degrees?)?\s?f(?:ahrenheit)?\b/gi;
+const fahrenheitToCelsius = f => ((f - 32) * 5 / 9).toFixed(2);
+let fMatch;
+while ((fMatch = fahrenheitRegex.exec(current.text)) !== null) {
+    const fahrenheit = parseFloat(fMatch);
+    const celsius = fahrenheitToCelsius(fahrenheit);
+    conversions.push(`${formatNumber(fahrenheit)}°F is ${formatNumber(celsius)} degrees in sane units (Celsius).`);
+}a
 
 const conversionMessage = conversions.join('\n');
 

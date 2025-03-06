1e4aba179b3e534067e9eae1e1cd00aff77f4c49
Chris Helming <7746625+chelming@users.noreply.github.com>
2025-03-06 09:22:43 -0500

    Update Check if user is verified.js

--- Parsers/Check if user is verified.js
+++ Parsers/Check if user is verified.js
@@ -12,11 +12,11 @@ user_id = user_id.replace(/>.*/, '').trim();
 
 var grUser = new GlideRecord('x_snc_slackerbot_user');
 if(grUser.get('user_id',user_id) && Object.keys(grUser).indexOf('verified') != -1){
-    verification_status = (grUser.getValue('verified') == 1 ? true : false) ? 'has been verified and confirmed by the Slack admins.' : 'has not been verified and confirmed by the Slack admins.';
+    verification_status = (grUser.getValue('verified') == 1 ? true : false) ? '*has been verified* and confirmed by the Slack admins.' : '*has not been verified* and confirmed by the Slack admins.';
     user_info = grUser.getValue('user_info') !== null ? grUser.getValue('user_info').toString() : '';
 
     message_body += '' + grUser.getValue('name') + '\'s identity '+ verification_status;
-    if(!gs.nil(user_info)) message_body += '\nThe following information has been noted about this user: ' + user_info;
+    if(!gs.nil(user_info)) message_body += '\n\nThe following information has been noted about this user:\n\n> ' + user_info;
 }
 else {
   message_body += 'I\'m afraid I can\'t do that. Either the user does not exist, or the logic to support this functionality is not yet on the instance.';
